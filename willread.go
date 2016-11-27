52643ceea27d481609edc5b5a890c27dbab63733
parallellink <parallellink@outlook.com>
2016-11-27 19:44:42 +0900

    Add "GET /list" to list links

--- willread.go
+++ willread.go
@@ -7,10 +7,24 @@ import (
 	"net/http"
 )
 
+var store LinkStore
+
 type Link struct {
 	Title string   `json:"title"`
 	Link  string   `json:"link"`
-	Tag   []string `json:"tag,string"`
+	Tag   []string `json:"tag"`
+}
+
+type LinkStore struct {
+	links []*Link
+}
+
+func (s *LinkStore) Add(l *Link) {
+	s.links = append(s.links, l)
+}
+
+func (s *LinkStore) List() []*Link {
+	return s.links
 }
 
 func newLink(w http.ResponseWriter, r *http.Request) {
@@ -21,19 +35,30 @@ func newLink(w http.ResponseWriter, r *http.Request) {
 
 	decoder := json.NewDecoder(r.Body)
 
-	var rec Link
-	err := decoder.Decode(&rec)
+	var rec *Link
 
-	if err != nil {
+	if err := decoder.Decode(&rec); err != nil {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
 	}
 
+	store.Add(rec)
+
 	fmt.Println(rec)
+}
+
+func listLink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	links := store.List()
+	fmt.Println(links)
+	json.NewEncoder(w).Encode(links)
 }
 
 func main() {
 	http.HandleFunc("/new", newLink)
+	http.HandleFunc("/list", listLink)
 
+	// storage
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
